f1b99711e4a670a9431c0999f60214b4100062fc
Lisa <lisaqdebona@gmail.com>
2025-10-02 03:33:58 -0400

    Styling Contact page

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="<?php bloginfo("template_url") ?>/css/jquery.fancybox.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script>var pageTitle = '<?php get_the_title(); ?>'</script>
--- page-contact.php
+++ page-contact.php
@@ -7,9 +7,102 @@ get_header(); ?>
 
 <div id="primary" class="content-area-full contact-page-content">
 	<main id="main" class="site-main" role="main">
-  <?php while ( have_posts() ) : the_post(); ?>
-    <div class="entry-content">
-      <?php the_content() ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+    <div class="midcontent wrapper">
+      <div class="inner-content">
+        <?php  
+        $title = get_field('title');
+        $staff = get_field('manager');
+        $details = get_field('contact_details');
+        $address = (isset($details['address']) && $details['address']) ? $details['address'] : '';
+        $phone = (isset($details['phone']) && $details['phone']) ? $details['phone'] : '';
+        $map = (isset($details['map']) && $details['map']) ? $details['map'] : '';
+        $hours = (isset($details['hours']) && $details['hours']) ? $details['hours'] : '';
+        ?>
+        <div class="infoCol detailCol">
+          <?php if ($title) { ?>
+          <h2 class="h2"><span><?php echo $title ?></span></h2> 
+          <?php } ?>
+          <?php if($staff) { 
+            $staffName = $staff->post_title;
+            $staffPhoto = get_field('photo', $staff->ID);
+            $staffJobTitle = get_field('job_title', $staff->ID);
+            ?>
+            <div class="staff-info">
+              <?php if ($staffPhoto) { ?>
+              <figure class="photo">
+                <img src="<?php echo $staffPhoto['url'] ?>" alt="" />
+              </figure>    
+              <?php } ?>
+              <div class="info">
+                <?php if ($staffJobTitle) { ?>
+                <h3 class="h3 jobtitle"><?php echo $staffJobTitle ?></h3>
+                <?php } ?>
+                <div class="name"><?php echo $staffName ?></div>
+              </div>
+            </div>
+            <?php } ?>
+
+            <div class="info-row">
+
+              <div class="group">
+                <?php if($address) { ?>
+                <div class="info flex address">
+                  <span class="icon -map"></span>
+                  <span class="text"><?php echo $address ?></span>
+                </div>
+                <?php } ?>
+
+                <?php if($phone) { ?>
+                <div class="info flex phone">
+                  <span class="icon -phone"></span>
+                  <span class="text"><?php echo $phone ?></span>
+                </div>
+                <?php } ?>
+              </div>
+
+              <?php if($hours) { ?>
+              <div class="info flex hours">
+                <span class="icon -hours"></span>
+                <span class="text">
+                  <?php foreach ($hours as $h) { 
+                    $h_title = $h['title'];
+                    $h_schedules= $h['schedules'];
+                    if($h_title || $h_schedules) { ?>
+                      <div class="sched">
+                        <?php if($h_title) { ?>
+                        <h3 class="h3"><?php echo $h_title ?></h3>
+                        <?php } ?>
+                        <?php if($h_schedules) { ?>
+                        <div class="schedules">
+                          <?php foreach ($h_schedules as $s) { 
+                            $days = $s['days'];
+                            $times = $s['times'];
+                            ?>
+                            <?php if($days) { ?>
+                            <div class="days"><strong><?php echo $days ?></strong></div>
+                            <?php } ?>
+                            <?php if($times) { ?>
+                            <div class="times"><?php echo $times ?></div>
+                            <?php } ?>
+                          <?php } ?>
+                        </div>
+                        <?php } ?>
+                      </div>
+                    <?php } ?>
+                  <?php } ?>
+                </span>
+              </div>
+              <?php } ?>
+            </div>
+          </div>
+
+        <?php if($map) { ?>
+        <div class="infoCol mapCol">
+          <div class="mapdiv"><?php echo $map ?></div>
+        </div>
+        <?php } ?>
+      </div>
     </div>
   <?php endwhile; ?>
   </main>
