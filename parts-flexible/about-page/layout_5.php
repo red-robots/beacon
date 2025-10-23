90f47397604341c8bc1970b149a97069f37ea4de
Mindy <mindy@valhallaonlineservices.com>
2025-10-23 18:23:50 +0800

    About us Page

--- parts-flexible/about-page/layout_5.php
+++ parts-flexible/about-page/layout_5.php
@@ -1,7 +1,6 @@
 <?php if( get_row_layout() == 'layout_5' ) {
   $story_title = get_sub_field('our_story_title');
   $section = get_sub_field('section');
-
   $has_content = ($story_title || $section) ? true : false;
   $custom_class = ($ctr==1) ? ' first':'';
   if ($has_content) { ?>
@@ -11,31 +10,42 @@
       <h2><?php echo $story_title; ?></h2>
 
       <?php if($section){
+        $count = 1;
+
         foreach($section as $sec){
-          print_r($sec);
+          $image_position = $sec['image_position'];
+          $content = $sec['text_content'];
+          $text_content = apply_filters('the_content', $content);
+          $image = $sec['featured_image'];
+          $imageUrl = $image['url'];
+          $imageTitle = $image['title'];
+          //print_r($sec);
+      ?>
+          <div class="flexwrap image-<?php echo $image_position; ?>">
+            <?php if($imageUrl) { ?>
+              <div class="fxcol imageCol image-<?php echo $count; ?>">
+                <figure class="imgwrap">
+                  <?php if ($count==1) { ?>
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/story-image-1.png" alt="" aria-hidden="true" role="presentation" class="hexagon" style="pointer-events:none;">
+                  <?php } else { ?>
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/story-image-2.png" alt="" aria-hidden="true" role="presentation" class="circle" style="pointer-events:none;">
+                  <?php } ?>
+                  <div class="inner">
+                    <img src="<?php echo $imageUrl ?>" alt="<?php echo $imageTitle ?>">
+                  </div> 
+                </figure>
+              </div>
+            <?php } ?>
+            <div class="fxcol textCol">
+              <?php echo $text_content; ?>
+            </div>
+          </div>
+      <?php
+          $count++;
         }
       ?>
 
       <?php } ?>
-
-      <!-- <div class="flexwrap">
-        <?php if ($image) { ?>
-          <div class="fxcol imageCol">
-            <div class="images">
-              <figure class="imgwrap first">
-                <div class="inner">
-                  <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['title'] ?>">
-                </div> 
-              </figure>
-            </div>
-          </div>
-        <?php } ?>
-        <?php if ($text) { ?>
-          <div class="fxcol textCol">
-            <?php echo anti_email_spam($text); ?>
-          </div>
-        <?php } ?>
-      </div> -->
     </div>  
   </section>
   <?php } ?>
